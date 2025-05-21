09cd00cb76d199b8cb426ae6441a6dfa510d94f1
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-05-21 22:17:34 +0000

    Added ohm_laws.py

--- ohm_laws.py
+++ ohm_laws.py
@@ -3,7 +3,7 @@
 # Date: April 19, 2025
 # This is an Ohm's law calculator that gets the resistance, voltage ,and current, depending on what the user wants
 # It uses different functions for each option
-# It gets all the user input inthe  main
+# It gets all the user input in the main
 
 
 # Function to calculate current: I = V / R
